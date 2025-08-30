800a150af3ade031962fa41b49a696c52d01b774
atif waqar <atif.sheikh87@gmail.com>
2025-08-30 14:17:37 +0200

    wired

---
+++ assets/js/render.js
@@ -0,0 +1,190 @@
+
+(function(){
+  const PRODUCT_CARD_TEMPLATE = `<li class="entry content-bg loop-entry product type-product post-145 status-publish first instock product_cat-fashion product_cat-mens-clothes product_cat-our-store product_cat-face-wash product_cat-sneakers-shoes product_cat-sunglasses product_cat-womens-clothes has-post-thumbnail sale shipping-taxable purchasable product-type-variable has-default-attributes cart-button-1 action-style-default product-hover-style1 image-hover-icon hover-right splide__slide is-active is-visible" id="splide05-slide01" role="group" aria-roledescription="slide" aria-label="1 of 8" >
+	<div class="product-thumbnail"><a href="https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/product/adro-men-print-regular-fit-hoodie-for-men/" class="woocommerce-loop-image-link woocommerce-LoopProduct-link woocommerce-loop-product__link product-has-hover-image" aria-label="ADRO Men Print Regular Fit Hoodie For Men">
+	<div class="product-onsale">
+                            <span aria-hidden="true" class="onsale ">-4%</span>
+                            <span class="screen-reader-text">Product on sale</span>
+                        </div>
+	<img loading="lazy" decoding="async" width="300" height="300" src="https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20-300x300.jpg" class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail" alt="ADRO Men Print Regular Fit Hoodie For Men" srcset="https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20-300x300.jpg 300w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20-150x150.jpg 150w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20-768x768.jpg 768w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20-600x600.jpg 600w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20-100x100.jpg 100w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20-96x96.jpg 96w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20-460x460.jpg 460w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20-236x236.jpg 236w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20-118x118.jpg 118w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20-700x700.jpg 700w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20-346x346.jpg 346w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20-173x173.jpg 173w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20-296x296.jpg 296w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20-148x148.jpg 148w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20.jpg 1000w" sizes="(max-width: 300px) 100vw, 300px"><img loading="lazy" decoding="async" width="300" height="300" src="https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/02-37-300x300.jpg" class="secondary-product-image attachment-woocommerce_thumbnail attachment-shop-catalog wp-post-image wp-post-image--secondary" alt="" title="02" srcset="https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/02-37-300x300.jpg 300w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/02-37-150x150.jpg 150w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/02-37-768x768.jpg 768w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/02-37-600x600.jpg 600w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/02-37-100x100.jpg 100w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/02-37-96x96.jpg 96w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/02-37-460x460.jpg 460w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/02-37-236x236.jpg 236w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/02-37-118x118.jpg 118w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/02-37-700x700.jpg 700w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/02-37-346x346.jpg 346w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/02-37-173x173.jpg 173w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/02-37-296x296.jpg 296w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/02-37-148x148.jpg 148w, https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/02-37.jpg 1000w" sizes="(max-width: 300px) 100vw, 300px"></a><div class="product-actions"><button class="woosw-btn woosw-btn-145 woosw-btn-has-icon woosw-btn-icon-text" data-id="145" data-product_name="ADRO Men Print Regular Fit Hoodie For Men" data-product_image="https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20-150x150.jpg" aria-label="Wishlist"><span class="woosw-btn-icon woosw-icon-5"></span><span class="woosw-btn-text">Wishlist</span></button><button class="woosc-btn woosc-btn-145  woosc-btn-has-icon woosc-btn-icon-text" data-id="145" data-product_name="ADRO Men Print Regular Fit Hoodie For Men" data-product_image="https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/wp-content/uploads/2024/01/20-150x150.jpg"><span class="woosc-btn-icon woosc-icon-1"></span><span class="woosc-btn-text">Compare</span></button><button class="woosq-btn woosq-btn-145 woosq-btn-has-icon woosq-btn-icon-text" data-id="145" data-effect="mfp-3d-unfold" data-context="default"><span class="woosq-btn-icon woosq-icon-1"></span><span class="woosq-btn-text">Quick view</span></button></div></div><div class="product-details content-bg entry-content-wrap"><h2 class="woocommerce-loop-product__title"><a href="https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/product/adro-men-print-regular-fit-hoodie-for-men/" class="woocommerce-LoopProduct-link-title woocommerce-loop-product__title_link">ADRO Men Print Regular Fit Hoodie For Men</a></h2><div class="star-rating-block"><div class="star-rating"><span ><strong class="rating">4.00</strong> out of 5</span></div></div>
+	<span class="price"><span class="price"><del aria-hidden="true"><span class="woocommerce-Price-amount amount"><bdi><span class="woocommerce-Price-currencySymbol">$</span>24</bdi></span></del> <span class="screen-reader-text">Original price was: $24.</span><ins aria-hidden="true"><span class="woocommerce-Price-amount amount"><bdi><span class="woocommerce-Price-currencySymbol">$</span>23</bdi></span></ins><span class="screen-reader-text">Current price is: $23.</span></span></span>
+<div class="product-action-wrap style-button"><a href="https://demos.codezeel.com/wordpress/WCM05/WCM050122/default/product/adro-men-print-regular-fit-hoodie-for-men/" data-quantity="1" class="button product_type_variable add_to_cart_button default" data-product_id="145" data-product_sku="GTP-IHG-246" aria-label="Select options for “ADRO Men Print Regular Fit Hoodie For Men”" rel="nofollow"><span class="add-to-cart-icon"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" fill="currentColor" version="1.1"><g id="surface1"><path d="M 14.527344 2.125 C 14.414062 2.183594 14.257812 2.324219 14.179688 2.433594 C 14.054688 2.617188 14.039062 2.667969 14.039062 2.996094 C 14.039062 3.3125 14.054688 3.378906 14.164062 3.542969 C 14.234375 3.640625 14.371094 3.777344 14.46875 3.84375 L 14.644531 3.960938 L 16.613281 3.976562 L 18.582031 3.988281 L 14.382812 8.195312 C 9.941406 12.632812 9.984375 12.585938 9.984375 13.007812 C 9.984375 13.203125 10.121094 13.550781 10.257812 13.707031 C 10.378906 13.847656 10.785156 14.015625 10.992188 14.015625 C 11.417969 14.011719 11.371094 14.054688 15.804688 9.617188 L 20.011719 5.417969 L 20.023438 7.386719 L 20.039062 9.355469 L 20.15625 9.53125 C 20.222656 9.628906 20.359375 9.765625 20.457031 9.835938 C 20.621094 9.945312 20.6875 9.960938 21 9.960938 C 21.3125 9.960938 21.378906 9.945312 21.542969 9.835938 C 21.640625 9.765625 21.777344 9.628906 21.84375 9.53125 L 21.960938 9.355469 L 21.960938 2.640625 L 21.835938 2.457031 C 21.765625 2.359375 21.628906 2.222656 21.53125 2.15625 L 21.355469 2.039062 L 18.046875 2.023438 L 14.738281 2.015625 Z M 14.527344 2.125 "></path><path d="M 4.777344 4.035156 C 3.488281 4.226562 2.433594 5.183594 2.105469 6.460938 C 2.019531 6.796875 2.015625 7.035156 2.015625 13.003906 C 2.015625 19.914062 1.996094 19.460938 2.351562 20.203125 C 2.507812 20.53125 2.621094 20.675781 2.972656 21.027344 C 3.324219 21.378906 3.46875 21.492188 3.796875 21.648438 C 4.539062 22.003906 4.085938 21.984375 10.996094 21.984375 C 16.960938 21.984375 17.203125 21.980469 17.539062 21.894531 C 18.6875 21.601562 19.550781 20.75 19.902344 19.566406 C 19.976562 19.3125 19.988281 18.945312 20.007812 15.515625 C 20.015625 13.4375 20.007812 11.628906 19.992188 11.5 C 19.929688 11.011719 19.566406 10.648438 19.078125 10.601562 C 18.683594 10.566406 18.355469 10.738281 18.132812 11.101562 L 18.023438 11.273438 L 18 15.1875 C 17.976562 19.09375 17.976562 19.101562 17.875 19.292969 C 17.738281 19.546875 17.503906 19.773438 17.25 19.894531 L 17.039062 19.992188 L 4.945312 19.992188 L 4.726562 19.882812 C 4.472656 19.757812 4.199219 19.476562 4.085938 19.222656 C 4.011719 19.070312 4.007812 18.53125 4.007812 13.007812 L 4.007812 6.960938 L 4.105469 6.75 C 4.226562 6.496094 4.453125 6.261719 4.707031 6.125 C 4.898438 6.023438 4.921875 6.023438 8.347656 6 L 11.792969 5.976562 L 11.992188 5.84375 C 12.273438 5.664062 12.425781 5.40625 12.453125 5.078125 C 12.488281 4.703125 12.308594 4.355469 11.976562 4.148438 L 11.75 4.007812 L 8.394531 4.003906 C 6.546875 4 4.921875 4.011719 4.777344 4.035156 Z M 4.777344 4.035156 "></path></g></svg></span><span class="cart-text">Select options</span><span class="base-svg-iconset svg-baseline"><svg aria-hidden="true" class="base-svg-icon base-arrow-right-alt-svg" fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" width="27" height="28" viewBox="0 0 27 28"><title>Continue</title><path d="M27 13.953c0 0.141-0.063 0.281-0.156 0.375l-6 5.531c-0.156 0.141-0.359 0.172-0.547 0.094-0.172-0.078-0.297-0.25-0.297-0.453v-3.5h-19.5c-0.281 0-0.5-0.219-0.5-0.5v-3c0-0.281 0.219-0.5 0.5-0.5h19.5v-3.5c0-0.203 0.109-0.375 0.297-0.453s0.391-0.047 0.547 0.078l6 5.469c0.094 0.094 0.156 0.219 0.156 0.359v0z"></path>
+				</svg></span><span class="base-svg-iconset svg-baseline"><svg class="base-svg-icon base-spinner-svg" fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><title>Loading</title><path d="M16 6h-6l2.243-2.243c-1.133-1.133-2.64-1.757-4.243-1.757s-3.109 0.624-4.243 1.757c-1.133 1.133-1.757 2.64-1.757 4.243s0.624 3.109 1.757 4.243c1.133 1.133 2.64 1.757 4.243 1.757s3.109-0.624 4.243-1.757c0.095-0.095 0.185-0.192 0.273-0.292l1.505 1.317c-1.466 1.674-3.62 2.732-6.020 2.732-4.418 0-8-3.582-8-8s3.582-8 8-8c2.209 0 4.209 0.896 5.656 2.344l2.343-2.344v6z"></path>
+				</svg></span><span class="base-svg-iconset svg-baseline"><svg class="base-svg-icon base-check-svg" fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><title>Done</title><path d="M14 2.5l-8.5 8.5-3.5-3.5-1.5 1.5 5 5 10-10z"></path>
+				</svg></span></a>	<span id="woocommerce_loop_add_to_cart_link_describedby_145" class="screen-reader-text">
+		This product has multiple variants. The options may be chosen on the product page	</span>
+</div></div></li>`;
+
+  function formatPrice(price, currency){
+    try{ price = Number(price||0); }catch(e){ price = 0; }
+    if(currency === 'PKR') return '₨ ' + Math.round(price).toLocaleString();
+    if(currency === 'USD') return '$' + (price/100).toFixed(2);
+    if(currency === 'EUR') return '€' + (price/100).toFixed(2);
+    return price.toLocaleString() + ' ' + (currency||'');
+  }
+
+  function htmlToElement(html){
+    const tpl = document.createElement('template');
+    tpl.innerHTML = html.trim();
+    return tpl.content.firstElementChild;
+  }
+
+  function createProductCard(p){
+    const li = htmlToElement(PRODUCT_CARD_TEMPLATE);
+
+    // Product link
+    const link = li.querySelector('a.woocommerce-LoopProduct-link') || li.querySelector('a');
+    if(link) {
+      link.href = 'product.html?slug=' + encodeURIComponent(p.slug || p.id);
+      link.setAttribute('aria-label', p.name || '');
+    }
+
+    // Title
+    const titleEl = li.querySelector('.woocommerce-loop-product__title');
+    if(titleEl) titleEl.textContent = p.name || '';
+
+    // Image
+    const img = li.querySelector('.product-thumbnail img') || li.querySelector('img');
+    if(img) {
+      const src = (p.images && p.images[0]) ? p.images[0] : '';
+      if(src) img.src = src;
+      img.alt = p.name || '';
+      // remove srcset/sizes if present to avoid external CDN references
+      img.removeAttribute('srcset');
+      img.removeAttribute('sizes');
+    }
+
+    // Price
+    const priceEl = li.querySelector('.price, .woocommerce-Price-amount, .amount');
+    if(priceEl) {
+      const amountEl = priceEl.querySelector('.woocommerce-Price-amount') || priceEl;
+      amountEl.textContent = formatPrice(p.price, p.currency);
+    }
+
+    // Badges / sale
+    const onsale = li.querySelector('.product-onsale, .onsale');
+    if(onsale) onsale.remove(); // hide theme sale badge unless you implement compare_at_price
+
+    // Any demo links/images cleanup
+    li.querySelectorAll('a, img').forEach(el => {
+      const attrs = ['href','src'];
+      attrs.forEach(attr => {
+        if(el.hasAttribute(attr) && /demos\.codezeel\.com/.test(el.getAttribute(attr))) {
+          if(attr==='href') el.setAttribute(attr, link ? link.href : '#');
+          else el.removeAttribute(attr);
+        }
+      });
+    });
+
+    return li;
+  }
+
+  function renderList(container, products, maxItems){
+    if(!container) return;
+    container.innerHTML = '';
+    const ul = container.matches('ul') ? container : container.querySelector('ul.products, ul');
+    const target = ul || container;
+    (products || []).slice(0, maxItems || products.length).forEach(p => {
+      target.appendChild(createProductCard(p));
+    });
+  }
+
+  async function bootstrapHome(){
+    const prods = await window.DataAPI.loadProducts();
+
+    // Featured
+    document.querySelectorAll('[data-bind="featured-products"]').forEach(box => {
+      const items = prods.filter(p => (p.tags||[]).includes('featured'));
+      renderList(box, items.length ? items : prods.slice(0, 12), 12);
+    });
+
+    // Bestsellers
+    document.querySelectorAll('[data-bind="bestseller-products"]').forEach(box => {
+      const items = prods.filter(p => (p.tags||[]).includes('bestseller') || (p.tags||[]).includes('best seller'));
+      renderList(box, items.length ? items : prods.slice(0, 12), 12);
+    });
+
+    // Deal of the Day (single)
+    document.querySelectorAll('[data-bind="deal-product"]').forEach(box => {
+      let items = prods.filter(p => (p.tags||[]).includes('deal') || (p.tags||[]).includes('hot'));
+      const pick = (items[0] || prods[0]);
+      renderList(box, [pick], 1);
+    });
+
+    // Categories (top-level from categories.json)
+    document.querySelectorAll('[data-bind="category-list"]').forEach(async box => {
+      const cats = await window.DataAPI.loadCategories();
+      const top = cats.filter(c => !c.parent);
+      const ul = box.matches('ul') ? box : box.querySelector('ul') || box;
+      ul.innerHTML = '';
+      top.forEach(c => {
+        const li = document.createElement('li');
+        li.className = 'menu-item menu-item-type-custom menu-item-object-custom';
+        const a = document.createElement('a');
+        a.href = 'shop.html?category=' + encodeURIComponent(c.slug);
+        a.textContent = c.name;
+        li.appendChild(a);
+        ul.appendChild(li);
+      });
+    });
+  }
+
+  async function bootstrapProduct(){
+    const params = new URLSearchParams(location.search);
+    const slug = params.get('slug');
+    const prods = await window.DataAPI.loadProducts();
+    const p = window.DataAPI.findBySlug(prods, slug);
+    if(!p) return;
+
+    // Title
+    const titleEl = document.querySelector('.product_title');
+    if(titleEl) titleEl.textContent = p.name;
+
+    // Price
+    const priceEl = document.querySelector('.summary .price .woocommerce-Price-amount, .summary .price, .price .woocommerce-Price-amount');
+    if(priceEl) priceEl.textContent = formatPrice(p.price, p.currency);
+
+    // SKU
+    const skuEl = document.querySelector('.sku');
+    if(skuEl) skuEl.textContent = p.sku || p.id;
+
+    // Images: replace main image
+    const mainImg = document.querySelector('.woocommerce-product-gallery img');
+    if(mainImg) {
+      const src = (p.images && p.images[0]) ? p.images[0] : '';
+      if(src) mainImg.src = src;
+      mainImg.alt = p.name || '';
+      mainImg.removeAttribute('srcset'); mainImg.removeAttribute('sizes');
+    }
+
+    // Breadcrumb/product link canonical
+    const canonical = document.querySelector('link[rel="canonical"]');
+    if(canonical) canonical.href = location.href;
+  }
+
+  async function bootstrapSearchOrCategory(){
+    const params = new URLSearchParams(location.search);
+    const q = params.get('q');
+    const cat = params.get('category');
+    const prods = await window.DataAPI.loadProducts();
+    let items = [];
+    if(q) items = window.DataAPI.search(prods, q);
+    else if(cat) items = window.DataAPI.byCategory(prods, cat);
+    else items = prods.slice(0, 24);
+
+    const bins = document.querySelectorAll('[data-bind="search-results"], [data-bind="category-results"]');
+    bins.forEach(box => renderList(box, items, 48));
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const page = document.body || document.documentElement;
+    const isProduct = !!document.querySelector('body.single-product, .product_title');
+    const hasHomeBinds = document.querySelector('[data-bind="featured-products"], [data-bind="bestseller-products"], [data-bind="deal-product"], [data-bind="category-list"]');
+    const hasSearch = document.querySelector('[data-bind="search-results"], [data-bind="category-results"]');
+    if(hasHomeBinds) bootstrapHome();
+    if(isProduct) bootstrapProduct();
+    if(hasSearch) bootstrapSearchOrCategory();
+  });
+
+  window.RenderAPI = { createProductCard, renderList };
+})();
